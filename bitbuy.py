7c37c9cb8429e1fed60f89d4865c61dc1937eccb
pphj191 <phjokokok@naver.com>
2021-11-03 17:36:30 +0900

    buy파일만듬

---
+++ bitbuy.py
@@ -0,0 +1,14 @@
+
+import matplotlib.pyplot as plt
+
+
+def graphshow():
+    x = [1, 2, 3, 4, 5]
+    y = [5, 6, 7, 8, 9]
+
+    plt.plot(x, y)
+    plt.show()
+
+
+if __name__ == '__main__':
+    graphshow()
---
+++ mother.py
@@ -0,0 +1,5 @@
+
+import bitbuy
+
+bitbuy.graphshow()
+print("end")
