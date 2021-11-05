31db9d695eb04d72d059b2b4b37c1efa3fa3fdd4
pphj191 <phjokokok@naver.com>
2021-11-06 01:38:40 +0900

    now_price added

--- rebalance.py
+++ rebalance.py
@@ -1,7 +1,18 @@
 
-# 내정보 가져오기 >> 설정금액, 가상금액
+# 5초에 한번씩 실행될것
+
+# 리밸런싱에 쓸 종목 = ["", ""]
+using_market = ["ETH-KRW", ]
+
+# 내정보 가져오기 >> 설정금액, 가상금액 [{'market':'ETH', 'balance':300} , {}]
+coins_now = bit_inq_mybalance(using_market)
+
 # 현재가 가져오기
-# 리밸런싱 
+for coin in coins_now:
+    coin['price'] = bit_inq_price[coin['market']]
+
+
+# for (종목별) : 리밸런싱 >> 매매 >> 확인
 
 #결과출력
 #결과
